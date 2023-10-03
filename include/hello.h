50815a4a9dfc4d4fd1faa5d20215dbdf0761c0f2
Meharjeet Singh <meharjeet_singh@sfu.ca>
2023-10-02 19:04:56 -0700

    initial commit

---
+++ include/hello.h
@@ -0,0 +1,7 @@
+
+
+#include <stdio.h>
+
+void hello() {
+	printf("Hello, world! from hello.h\n");
+}
---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "hello.h"
+
+int main(int argc, char** argv) {
+	hello();
+
+	printf("Hello, world! from main.c\n");
+	return 0;
+}
